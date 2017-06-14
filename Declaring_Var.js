ff61206a5dae1a2867262c98eeddb8b3dfb84125
Dellydell19git config --global user.name Dellydell19git config --global user.name Dellydell19 <carolyntorain@yahoo.com>
2017-06-14 17:23:40 -0500

    I'm learning JavaScript Variables and Console.log

---
+++ Declaring_Var.js
@@ -0,0 +1,39 @@
+var myAge = 60;
+
+console.log(myAge);
+
+var wonderYears ="21";
+
+console.log(wonderYears);
+
+var dogAge = "90";
+
+console.log(dogAge);
+
+var carMiles =95040;
+
+console.log(carMiles);
+
+var string1 = "Mike";
+
+console.log("My first String:",string1);
+
+var string2 = "I Love Cats";
+
+console.log("My seconds string:", string2);
+
+var string3 = "My car is yellow";
+
+console.log("My third string:", string3);
+
+var dodgeRules = "true"
+
+console.log(dodgeRules);
+
+var threeWheels = "false"
+
+console.log(threeWheels);
+
+var undefined;
+
+console.log(undefined);
