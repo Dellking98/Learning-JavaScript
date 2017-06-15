1c03cabba34284d70b687505d0ad8168aa73dc5d
Dellydell19git config --global user.name Dellydell19git config --global user.name Dellydell19 <carolyntorain@yahoo.com>
2017-06-14 20:02:08 -0500

    I'm So happy I did A For Loop, It's My Partygit add .

---
+++ Birthday_party.js
@@ -0,0 +1,28 @@
+for (var daysUntilMyBirthday = 60; daysUntilMyBirthday > 30;
+  daysUntilMyBirthday = daysUntilMyBirthday - 1){
+
+  if(daysUntilMyBirthday >= 30 ){
+    console.log(daysUntilMyBirthday + "Days Until My Bday, This Sucks");
+  }
+}
+
+for(var daysUntilMyBirthday = 29; daysUntilMyBirthday > 29;
+  daysUntilMyBirthday = daysUntilMyBirthday - 1){
+
+  if( daysUntilMyBirthday >= 5){
+    console.log(daysUntilMyBirthday + "Day until My Bday, Not Long Now");
+  }
+}
+
+for (var daysUntilMyBirthday = 5; daysUntilMyBirthday > 0;
+  daysUntilMyBirthday = daysUntilMyBirthday - 1) {
+
+  if( daysUntilMyBirthday >= 0) {
+    console.log(daysUntilMyBirthday + "Day until My Bday, Who's Excited");
+  }
+
+}
+
+
+
+  console.log("It's My Party, I'll Cry If I Want TO!!!");
