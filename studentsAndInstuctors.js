7c64062a84ef82edd8993fc91ee9adb5d4f66c9d
Dellking98 <carolyntorain@yahoo.com>
2017-07-02 21:42:26 -0500

    Update studentsAndInstuctors.js

--- studentsAndInstuctors.js
+++ studentsAndInstuctors.js
@@ -20,10 +20,12 @@ function students1() {
 
 
    function functionName1() {
-
+        var i, s, x= "";
+        var text = "";
+        var text2 = "";
      var users = {
       'Students': [
-          {first_name:  'Michael', last_name : 'Jordan'},
+          {first_name: 'Michael', last_name : 'Jordan'},
           {first_name : 'John', last_name : 'Rosales'},
           {first_name : 'Mark', last_name : 'Guillen'},
           {first_name : 'KB', last_name : 'Tonel'}
@@ -33,12 +35,20 @@ function students1() {
           {first_name : 'Martin', last_name : 'Puryear'}
        ]
       }
-   for( var i in users){
+   for( i in users.Students){
+     text+= users.Students[i].first_name + " " + users.Students[i].last_name + " ";
+    //  console.log();
+  };
+    console.log(text);
 
-    console.log( users[i] );
+    for(s in users.Instructors){
+        text2+=users.Instructors[s].first_name + " " + users.Instructors[s].last_name + " ";
+      };
+        console.log(text2);
 
+      // console.log("1", users.Instructors[1].first_name, users.Students[0].last_name );
    }
 
- }
+
 
 functionName1();
